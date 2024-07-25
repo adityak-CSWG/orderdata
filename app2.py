d88f7aac3bde505881157f558a2f8d4bc08c15b9
AyushKarupakula <61338572+AyushKarupakula@users.noreply.github.com>
2024-07-24 22:15:52 -0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,7 +3,7 @@ import pandas as pd
 import altair as alt
 
 # Load the cleaned CSV file
-df = pd.read_csv('cleaned_warehouse_orders.csv')
+df = pd.read_csv('Cleaned_Warehouse_Orders_Data__New_.csv')
 
 # Convert 'date' column to datetime
 df['date'] = pd.to_datetime(df['date'])
