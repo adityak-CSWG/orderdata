5adb203681d61e7abbad6cf9d66d4f9814fe6156
Adi <adkumar@cswg.com>
2024-07-25 22:23:12 -0400

    bigquery connection plus added a config.toml file

--- app.py
+++ app.py
@@ -1,16 +1,54 @@
-import streamlit as st
 import pandas as pd
 import altair as alt
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
 
-# Load the cleaned CSV file
-df = pd.read_csv('Cleaned_Warehouse_Orders_Data__New_.csv')
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=["https://www.googleapis.com/auth/cloud-platform"]
+)
+client = bigquery.Client(credentials=credentials)
 
-# Create a time series chart
-chart = alt.Chart(df).mark_line(point=True).encode(
-    x='date:T',
+query = """
+WITH FlattenedOrders AS (
+  SELECT
+    ORDER_DATE,
+    o.trade_name,
+    o.warehouse_address,
+    o.num_orders
+  FROM
+    `gc-proj-devis-poc-6586.AppDev_Intern.customer-orders-2024`,
+    UNNEST(CUSTOMER_ORDERS) AS o
+)
+
+SELECT
+  ORDER_DATE,
+  trade_name,
+  warehouse_address,
+  num_orders
+FROM
+  FlattenedOrders"""
+
+@st.cache_data(ttl=600)
+def run_query():
+    query_job = client.query(query)
+    results = query_job.result()
+    data = results.to_dataframe()
+    return data
+
+df = run_query()
+
+df['ORDER_DATE'] = pd.to_datetime(df['ORDER_DATE'])
+
+# Ensure numeric conversion for 'num_orders'
+df['num_orders'] = pd.to_numeric(df['num_orders'])
+
+chart = alt.Chart(df).mark_line().encode(
+    x='ORDER_DATE:T',
     y='num_orders:Q',
-    color='warehouse_name:N',
-    tooltip=['date:T', 'warehouse_name:N', 'num_orders:Q']
+    color='warehouse_address:N',
+    tooltip=['ORDER_DATE:T', 'warehouse_address:N', 'num_orders:Q']
 ).interactive()
 
 # Display the chart in Streamlit
